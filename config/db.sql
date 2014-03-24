11989fbb2382664abe1c78d0763937ddafca04d4
Frengky <betzeika@gmail.com>
2014-03-24 15:12:03 +0700

    Update database structures

--- config/db.sql
+++ config/db.sql
@@ -5,11 +5,9 @@ CREATE TABLE IF NOT EXISTS `feeds` (
   `user_name` varchar(255) NOT NULL,
   `screen_name` varchar(255) NOT NULL,
   `rt_count` int(10) unsigned NOT NULL,
-  `tweet` text NOT NULL,
+  `tweet` varchar(255) NOT NULL,
   `tweeted` datetime DEFAULT NULL,
   `app_user` varchar(255) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
