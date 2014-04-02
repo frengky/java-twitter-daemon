29e56f0dc09ff570aa3cda83d1da517ecb9bd087
Frengky <betzeika@gmail.com>
2014-04-02 15:48:11 +0700

    Update db sql

--- config/db.sql
+++ config/db.sql
@@ -1,15 +1,16 @@
 CREATE TABLE IF NOT EXISTS `feeds` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
-  `user_name` varchar(255) NOT NULL,
-  `screen_name` varchar(255) NOT NULL,
-  `mention` varchar(255) NOT NULL,
-  `tweet` varchar(255) NOT NULL,
-  `timeline` varchar(255) NOT NULL,
-  `published` varchar(3) NOT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `screen_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `mention` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `tweet` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `timeline` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `radio_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `published` varchar(3) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `deleted` tinyint(1) NOT NULL,
   `tweeted` datetime NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
